5f6e823a67c93fdc85dd4d2f0314443a38ace317
user <user@WIN-NDE1EU06PGL>
2026-06-01 19:37:39 +0300

    да

--- backup-script.sh
+++ backup-script.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
-# Настройки
+
 BACKUP_DIR="/backups"
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-BACKUP_FILE="$BACKUP_DIR/dashboard_$TIMESTAMP.sql.gz"
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+BACKUP_FILE="${BACKUP_DIR}/dashboard_${TIMESTAMP}.sql.gz"
 
-# Создание бэкапа
-echo "Starting backup at $(date)"
-pg_dump -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE | gzip > "$BACKUP_FILE"
 
-# Проверка успешности
-if [ $? -eq 0 ]; then
-    echo "Backup completed: $BACKUP_FILE"
-    # Создаем симлинк на последний бэкап
-    ln -sf "$BACKUP_FILE" "$BACKUP_DIR/latest.sql.gz"
+echo "[$(date)] Starting backup..."
+pg_dump -h postgres -U postgres dashboard | gzip > "$BACKUP_FILE"
+
+
+if [ -s "$BACKUP_FILE" ]; then
+    echo "[$(date)] Backup created: $BACKUP_FILE ($(du -h "$BACKUP_FILE" | cut -f1))"
 else
-    echo "Backup failed!"
+    echo "[$(date)] ERROR: Backup failed!"
+    rm -f "$BACKUP_FILE"
 fi
